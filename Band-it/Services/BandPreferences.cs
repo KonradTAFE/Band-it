ea8861112c9ca8a0c06135bced91af233891537a
Konrad Krzyzkowiak <20147441@tafe.wa.edu.au>
2025-11-09 13:02:57 +0800

    feat: create BandPreferences service class and move all band operations

---
+++ Band-it/Services/BandPreferences.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Band_it.Modules;
+
+namespace Band_it.Services
+{
+    internal class BandPreferences
+    {
+        public ObservableCollection<Band> GetBands()
+        {
+            ObservableCollection<Band> bands = new ObservableCollection<Band>();
+            int count = Preferences.Get("BandCount", 0);
+            for (int i = 0; i < count; i++)
+            {
+                string color = Preferences.Get($"Band_{i}_Name", "");
+                int resistance = Preferences.Get($"Band_{i}_Resistance", 0);
+                if (color != "" && resistance > 0)
+                {
+                    bands.Add(new Band(color, resistance));
+                }
+            }
+            return bands;
+        }
+
+        public void SaveBands(ObservableCollection<Band> bands) 
+        {
+            Preferences.Set("BandCount", bands.Count);
+            for (int i = 0; i < bands.Count; i++)
+            {
+                Preferences.Set($"Band_{i}_Name", bands[i].Color);
+                Preferences.Set($"Band_{i}_Resistance", bands[i].Resistance);
+            }
+        }
+
+        public void LoadDefaultBands()
+        {
+            ObservableCollection<Band> defaults = new ObservableCollection<Band>
+        {
+            new Band("red", 10),
+            new Band("orange", 25),
+            new Band("black", 40),
+            new Band("purple", 55),
+            new Band("green", 70)
+        };
+
+            SaveBands(defaults);
+        }
+    }
+}
--- Band-it/Views/App_settings.xaml.cs
+++ Band-it/Views/App_settings.xaml.cs
@@ -1,11 +1,13 @@
 using System.Collections.ObjectModel;
 using Band_it.Modules;
+using Band_it.Services;
 
 namespace Band_it.Views;
 
 public partial class App_settings : ContentPage
 {
     private ObservableCollection<Band> _bands = new ObservableCollection<Band>();
+    BandPreferences _preferences = new BandPreferences();
     public App_settings()
     {
         InitializeComponent();
@@ -28,33 +30,10 @@ public partial class App_settings : ContentPage
     public void LoadBands()
     {
         _bands.Clear();
-
-        int count = Preferences.Get("BandCount", 0);
-        for (int i = 0; i < count; i++)
-        {
-            string color = Preferences.Get($"Band_{i}_Name", "");
-            int resistance = Preferences.Get($"Band_{i}_Resistance", 0);
-            if (color != "" && resistance > 0)
-            {
-                _bands.Add(new Band(color, resistance));
-            }
-        }
-
+        _bands = _preferences.GetBands();
         if (_bands.Count == 0)
         {
-            LoadDefaultBands();
-        }
-    }
-
-
-
-    private void SaveBands()
-    {
-        Preferences.Set("BandCount", _bands.Count);
-        for (int i = 0; i < _bands.Count; i++)
-        {
-            Preferences.Set($"Band_{i}_Name", _bands[i].Color);
-            Preferences.Set($"Band_{i}_Resistance", _bands[i].Resistance);
+            _preferences.LoadDefaultBands();
         }
     }
 
@@ -62,22 +41,9 @@ public partial class App_settings : ContentPage
     {
         if(sender is Button button && button.BindingContext is Band band)
         {
-            for (int i = 0; i < Preferences.Get("BandCount", 0); i++)
-            {
-                if (Preferences.Get($"Band_{i}_Name", "") == band.Color
-                    && Preferences.Get($"Band_{i}_Resistance", 0) == band.Resistance)
-                {
-                    Preferences.Remove($"Band_{i}_Name");
-                    Preferences.Remove($"Band_{i}_Resistance");
-                }
-            }
             _bands.Remove(band);
-            SaveBands();
-            
+            _preferences.SaveBands(_bands);
         }
-        //bands_list.ItemsSource = _bands;
-
-
     }
 
     private async void addBand_Clicked(object sender, EventArgs e)
@@ -85,21 +51,4 @@ public partial class App_settings : ContentPage
         await Navigation.PushAsync(new Add_band());
     }
 
-    private void LoadDefaultBands()
-    {
-        List<Band> defaults = new List<Band>
-        {
-            new Band("red", 10),
-            new Band("orange", 25),
-            new Band("black", 40),
-            new Band("purple", 55),
-            new Band("green", 70)
-        };
-
-        foreach (Band band in defaults)
-        {
-            _bands.Add(band);
-        }
-        SaveBands();
-    }
 }
